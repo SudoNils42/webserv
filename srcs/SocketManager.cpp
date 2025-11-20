b596e32b735844ca9e221b7322bfaf5b07337e50
Nils Bonnet <nbonnet@c2r7s3.42lausanne.ch>
2025-11-20 18:08:23 +0100

    fix POST in more than 8ko

--- srcs/SocketManager.cpp
+++ srcs/SocketManager.cpp
@@ -7,6 +7,7 @@ SocketManager::SocketManager(int port) {
     server_poll.fd = _server_fd;
     server_poll.events = POLLIN;
     _fds.push_back(server_poll);
+    _body_parts.push_back("");
     std::cout << "server listening on port " << port << std::endl;
 }
 
@@ -36,11 +37,13 @@ void SocketManager::addClient(int client_fd) {
     client_poll.fd = client_fd;
     client_poll.events = POLLIN;
     _fds.push_back(client_poll);
+    _body_parts.push_back("");
 }
 
 void SocketManager::removeClient(size_t i) {
     close(_fds[i].fd);
     _fds.erase(_fds.begin() + i);
+    _body_parts.erase(_body_parts.begin() + i);
 }
 
 void SocketManager::handleNewConnection() {
@@ -49,8 +52,31 @@ void SocketManager::handleNewConnection() {
 }
 
 void SocketManager::handleClientData(int client_fd) {
-    char buf[8192];
-    read(client_fd, buf, sizeof(buf) -1);
+    char buf[16];
+    size_t i;
+    for (i = 1; i < _fds.size(); i++)
+        if (_fds[i].fd == client_fd)
+            break;
+
+    ssize_t r = 1;
+    while (r > 0)
+    {
+        r = read(client_fd, buf, 15);
+        if (r > 0)
+            _body_parts[i].append(buf, r);
+    }
+
+    if (_body_parts[i].find("\r\n\r\n") == std::string::npos)
+        return;
+
+    if (_body_parts[i].find("Content-Length: ") != std::string::npos && _body_parts[i].find("\r\n\r\n") != std::string::npos)
+    {
+        size_t cl = _body_parts[i].find("Content-Length: ") + 16;
+        size_t body_len = atoi(_body_parts[i].c_str() + cl);
+        size_t body_start = _body_parts[i].find("\r\n\r\n") + 4;
+        if (_body_parts[i].size() - body_start < body_len)
+            return;
+    }
 
     std::string body = "<h1>Salut bg !</h1>";
     std::string resp =
